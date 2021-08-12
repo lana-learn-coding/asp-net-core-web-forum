0f13a68120eb86a731b5147a3f4828ba9aa7381f
toannd <sitdownrightnow.dev@gmail.com>
2021-08-12 09:55:56 +0700

    ui: initial commit

---
+++ Web/ClientApp/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': [
+      'warn',
+      { code: 150 },
+    ],
+    'class-methods-use-this': 'off',
+    'no-shadow': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'object-curly-newline': ['error', {
+      ImportDeclaration: {
+        multiline: true,
+        minProperties: 8,
+      },
+      ExportDeclaration: {
+        multiline: true,
+        minProperties: 8,
+      },
+    }],
+  },
+};
