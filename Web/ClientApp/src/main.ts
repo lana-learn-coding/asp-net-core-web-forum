c7429dd1e8cea73008d122bd971008c46a93285f
toannd <sitdownrightnow.dev@gmail.com>
2021-08-12 11:48:23 +0700

    ui: build: install plugins composition api and buefy to vue

--- Web/ClientApp/src/main.ts
+++ Web/ClientApp/src/main.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import './plugins/buefy';
+import './plugins/composition-api';
 
 Vue.config.productionTip = false;
 
