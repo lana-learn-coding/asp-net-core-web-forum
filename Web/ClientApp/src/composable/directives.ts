c05118917e7eb1b8f1da5f59654b37879ba4268e
toannd <sitdownrightnow.dev@gmail.com>
2021-08-26 07:15:14 +0700

    feat: add role-checking directive

--- Web/ClientApp/src/composable/directives.ts
+++ Web/ClientApp/src/composable/directives.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import { getDirective } from 'vue-debounce';
+import { useUser } from '@/services/auth';
 
 Vue.directive('debounce', getDirective('2', {
   listenTo: 'input',
@@ -7,3 +8,16 @@ Vue.directive('debounce', getDirective('2', {
   fireOnEmpty: false,
   trim: true,
 }));
+
+const { user } = useUser();
+Vue.directive('can', {
+  bind(el, binding) {
+    const role = binding.expression?.replace(/['"]/g, '') || '';
+    if (user.roles.includes(role)) {
+      return;
+    }
+    if (el.style?.display) {
+      el.style.display = 'none';
+    }
+  },
+});
