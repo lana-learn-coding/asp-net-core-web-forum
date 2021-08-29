71b641bf837e247d02a1d2863754a97ebb958bd6
toannd <sitdownrightnow.dev@gmail.com>
2021-08-30 01:56:34 +0700

    feat: get statistics from server

--- Web/ClientApp/src/components/app/AppForumStatistics.vue
+++ Web/ClientApp/src/components/app/AppForumStatistics.vue
@@ -3,37 +3,47 @@
     <v-card flat class="mb-4">
       <v-card-title>Statistics</v-card-title>
       <v-card-text>
-        <div class="mb-2">
-          <b>
-            <v-icon small class="mr-1">question_answer</v-icon>
-            <number-counter from="0" :to="statistics.totalPosts"></number-counter>
-          </b> Total Posts
-        </div>
-        <div class="mb-2">
-          <b>
-            <v-icon small class="mr-1">chat_bubble</v-icon>
-            <number-counter from="0" :to="statistics.totalThreads"></number-counter>
-          </b> Total Threads
-        </div>
-        <div class="mb-2">
-          <b>
-            <v-icon small class="mr-1">people</v-icon>
-            <number-counter from="0" :to="statistics.totalMembers"></number-counter>
-          </b> Members
-        </div>
-        <div>
-          <b>
-            <v-icon small class="mr-1">record_voice_over</v-icon>
-            <number-counter from="0" :to="statistics.onlineMembers"></number-counter>
-          </b> Online
-        </div>
+        <v-skeleton-loader type="text@6" v-if="loading.statistics"></v-skeleton-loader>
+        <template v-else>
+          <div class="mb-2">
+            <b>
+              <v-icon small class="mr-1">question_answer</v-icon>
+              <number-counter from="0" :to="statistics.totalPosts"></number-counter>
+            </b> Total Posts
+          </div>
+          <div class="mb-2">
+            <b>
+              <v-icon small class="mr-1">chat_bubble</v-icon>
+              <number-counter from="0" :to="statistics.totalThreads"></number-counter>
+            </b> Total Threads
+          </div>
+          <div class="mb-2">
+            <b>
+              <v-icon small class="mr-1">people</v-icon>
+              <number-counter from="0" :to="statistics.totalMembers"></number-counter>
+            </b> Members
+          </div>
+          <div class="mb-2">
+            <b>
+              <v-icon small class="mr-1">record_voice_over</v-icon>
+              <number-counter from="0" :to="statistics.onlineMembers"></number-counter>
+            </b> Online
+          </div>
+          <div>
+            <b>
+              <v-icon small class="mr-1">fiber_manual_record</v-icon>
+              <number-counter from="0" :to="statistics.onlineAnonymous"></number-counter>
+            </b> Anonymous
+          </div>
+        </template>
       </v-card-text>
     </v-card>
 
     <v-card flat class="mb-4">
       <v-card-title>Latest Threads</v-card-title>
       <v-card-text>
-
+        <v-skeleton-loader v-if="loading.threads" type="paragraph@2, sentences"></v-skeleton-loader>
+        <v-skeleton-loader v-else type="paragraph@2"></v-skeleton-loader>
       </v-card-text>
     </v-card>
     <v-card flat>
@@ -51,21 +61,46 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from '@vue/composition-api';
+import { defineComponent, onMounted, reactive } from '@vue/composition-api';
 import NumberCounter from '@/components/NumberCounter.vue';
+import { useHttp } from '@/services/http';
 
 export default defineComponent({
   name: 'AppForumStatistics',
   components: { NumberCounter },
   setup() {
+    const http = useHttp();
     const statistics = reactive({
-      totalMembers: 512433,
-      onlineMembers: 9578,
-      totalThreads: 5528,
-      totalPosts: 27694,
+      totalMembers: 0,
+      totalThreads: 0,
+      totalPosts: 0,
+      onlineMembers: 0,
+      onlineAnonymous: 0,
     });
+
+    const loading = reactive({
+      statistics: true,
+      threads: true,
+    });
+
+    onMounted(() => {
+      http
+        .get('/tracking/statistics')
+        .then((val) => Object.assign(statistics, val))
+        .finally(() => {
+          loading.statistics = false;
+        });
+
+      http
+        .get('/tracking/active-threads')
+        .finally(() => {
+          loading.threads = false;
+        });
+    });
+
     return {
       statistics,
+      loading,
     };
   },
 });
