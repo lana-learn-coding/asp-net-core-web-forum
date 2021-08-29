633eaf3cb8e845fb0d1730ad3794b69059281b7f
toannd <sitdownrightnow.dev@gmail.com>
2021-08-30 01:32:20 +0700

    feat: add mock tracking controller

---
+++ Web/Controllers/Public/TrackingController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DAL;
+using Microsoft.AspNetCore.Mvc;
+using Web.Dto.Home;
+
+namespace Web.Controllers.Public
+{
+    [ApiController]
+    [Route("/api/tracking")]
+    public class TrackingController : Controller
+    {
+        private const int TrackPeriod = 1000;
+
+        private readonly ModelContext _context;
+
+        public TrackingController(ModelContext context)
+        {
+            _context = context;
+        }
+
+        [Route("statistics")]
+        [HttpGet]
+        [ResponseCache(Duration = TrackPeriod)]
+        public JsonResult Statistic()
+        {
+            var totalUsers = _context.Users.Count();
+            return new JsonResult(new ForumStatistic
+                {
+                    TotalMembers = totalUsers,
+                    TotalPosts = 27694,
+                    TotalThreads = 5528,
+                    OnlineMembers = 5,
+                    OnlineAnonymous = 122
+                }
+            );
+        }
+
+        [Route("active-threads")]
+        [HttpGet]
+        [ResponseCache(Duration = TrackPeriod)]
+        public JsonResult ActiveThreads()
+        {
+            return new JsonResult(new List<string>());
+        }
+
+        [Route("logs")]
+        [HttpPost]
+        public ActionResult Track()
+        {
+            return new OkResult();
+        }
+    }
+}
---
+++ Web/Dto/Home/ForumStatistic.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.Dto.Home
+{
+    public class ForumStatistic
+    {
+        public int TotalMembers { get; set; }
+        public int OnlineMembers { get; set; }
+        public int OnlineAnonymous { get; set; }
+        public int TotalThreads { get; set; }
+        public int TotalPosts { get; set; }
+    }
+}
