a019731e34acf1b5c2aa497ed71ca2a9c610d213
toannd <sitdownrightnow.dev@gmail.com>
2021-09-04 22:46:43 +0700

    refactor: remove unnecessary fake data with default seeding
    - we will create fake data seeder later

--- DAL/Database/DatabaseInitializer.cs
+++ DAL/Database/DatabaseInitializer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
 using Bogus;
 using DAL.Models;
 using DAL.Models.Auth;
@@ -15,12 +14,45 @@ namespace DAL.Database
         protected override void Seed(ModelContext context)
         {
             base.Seed(context);
-            SeedUsers(context);
             SeedTopic(context);
-            context.SaveChanges();
-
+            SeedUsers(context);
             SeedForums(context);
             context.SaveChanges();
+        }
+
+        private static void SeedTopic(ModelContext context)
+        {
+            context.Categories.AddRange(new[]
+            {
+                new Category { Name = "General Discussion", Priority = (short)Priority.High },
+                new Category { Name = "Question & Answer", Priority = (short)Priority.High },
+                new Category
+                {
+                    Name = "Announcements & FeedBack",
+                    Description =
+                        "All major announcements and news can be found in here. Keep up to date with regards to the development of the doctors community!",
+                    Priority = (short)Priority.VeryHigh
+                },
+                // this forum act as default forum mapping when deleted
+                new Category
+                {
+                    Name = "Uncategorized",
+                    Description = "Uncategorized forums",
+                    Id = Guid.Empty, Priority = (short)Priority.Low
+                }
+            });
+
+            context.Tags.AddRange(new[]
+            {
+                new Tag { Name = "Neurology" },
+                new Tag { Name = "Surgery" },
+                new Tag { Name = "Urology" },
+                new Tag { Name = "Infectious Diseases" },
+                new Tag { Name = "Hematology" },
+                new Tag { Name = "Dermatology" },
+                new Tag { Name = "Endocrinology & Diabetes" },
+                new Tag { Name = "Gastroenterology & Liver Diseases" }
+            });
         }
 
         private static void SeedUsers(ModelContext context)
@@ -50,57 +82,21 @@ namespace DAL.Database
                     Email = "test@test.com"
                 }
             );
-
-            for (var i = 1; i <= 20; i++)
-            {
-                faker = new Faker();
-                context.Users.Add(new User
-                    {
-                        Avatar = faker.Person.Avatar,
-                        Username = faker.Person.UserName + i,
-                        Password = BCrypt.Net.BCrypt.HashPassword("1"),
-                        Email = faker.Person.Email + i,
-                    }
-                );
-            }
-        }
-
-        private static void SeedTopic(ModelContext context)
-        {
-            context.Categories.AddRange(new[]
-            {
-                new Category { Name = "General Discussion", Priority = (short)Priority.High },
-                new Category { Name = "Question & Answer", Priority = (short)Priority.High },
-                new Category
+            // this user act as default user mapping when deleted
+            context.Users.Add(new User
                 {
-                    Name = "Announcements & FeedBack",
-                    Description = "All major announcements and news can be found in here. Keep up to date with regards to the development of the doctors community!",
-                    Priority = (short)Priority.VeryHigh
-                },
-                new Category
-                {
-                    Name = "Uncategorized",
-                    Description = "Uncategorized forums",
-                    Id = Guid.Empty, Priority = (short)Priority.Low
+                    Id = Guid.Empty,
+                    Avatar = faker.Person.Avatar,
+                    Username = "Anon",
+                    Password = BCrypt.Net.BCrypt.HashPassword("1"),
+                    Email = "anon@anon.com"
                 }
-            });
-
-            context.Tags.AddRange(new[]
-            {
-                new Tag { Name = "Neurology" },
-                new Tag { Name = "Surgery" },
-                new Tag { Name = "Urology" },
-                new Tag { Name = "Infectious Diseases" },
-                new Tag { Name = "Hematology" },
-                new Tag { Name = "Dermatology" },
-                new Tag { Name = "Endocrinology & Diabetes" },
-                new Tag { Name = "Gastroenterology & Liver Diseases" }
-            });
+            );
         }
 
         private static void SeedForums(ModelContext context)
         {
-            var categories = context.Categories.ToList();
+            // this forum act as default forum mapping when deleted
             context.Forums.Add(new Forum
                 {
                     Id = Guid.Empty,
@@ -112,21 +108,6 @@ namespace DAL.Database
                     ThreadAccess = AccessMode.Private
                 }
             );
-            for (var i = 0; i < 30; i++)
-            {
-                var faker = new Faker();
-                context.Forums.Add(new Forum
-                    {
-                        Title = $"{faker.Company.CatchPhrase()} {i}",
-                        SubTitle = $"{faker.Name.JobDescriptor()} {faker.Name.JobTitle()}",
-                        Description = faker.Lorem.Sentence(10, 10),
-                        Priority = faker.Random.Short((short)Priority.Normal, (short)Priority.VeryHigh),
-                        CategoryId = categories[faker.Random.Int(0, 3)].Id,
-                        ForumAccess = AccessMode.Public,
-                        ThreadAccess = AccessMode.Public
-                    }
-                );
-            }
         }
     }
 }
