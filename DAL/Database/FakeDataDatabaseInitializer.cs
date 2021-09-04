fa2c54437252f9fc71d49359115d0dbe50744d42
toannd <sitdownrightnow.dev@gmail.com>
2021-09-05 02:48:03 +0700

    feat: update data faking progress to adapt new model schema

--- DAL/Database/FakeDataDatabaseInitializer.cs
+++ DAL/Database/FakeDataDatabaseInitializer.cs
@@ -16,10 +16,12 @@ namespace DAL.Database
 
             Console.WriteLine("Start generating fake data...");
             FakeUsers(context);
-            FakeForums(context);
-            FakeThreads(context);
+            context.SaveChanges();
 
-            Console.WriteLine("Saving fake data...");
+            FakeForums(context);
+            context.SaveChanges();
+
+            FakeThreads(context);
             context.SaveChanges();
             Console.WriteLine("Generation completed.");
         }
@@ -69,27 +71,29 @@ namespace DAL.Database
         {
             Console.WriteLine("Start generating threads:");
 
-            var users = context.Users.Local.ToList();
-            var forums = context.Forums.Local.ToList();
+            var users = context.Users.ToList();
+            var forums = context.Forums.ToList();
             var tags = context.Tags.ToList();
             for (var i = 0; i < 200; i++)
             {
+                var id = Guid.NewGuid();
                 var faker = new Faker();
+                var thread = new Thread
+                {
+                    Id = id,
+                    ForumId = forums[faker.Random.Int(0, forums.Count - 1)].Id,
+                    Title =
+                        $"{faker.Music.Genre() + faker.Commerce.ProductMaterial() + faker.Name.JobTitle() + faker.Company.CompanyName()} {i}",
+                    Tags = tags.Skip(faker.Random.Int(3, 8)).ToList()
+                };
                 var originPost = new Post
                 {
+                    Id = id,
+                    ThreadId = id,
                     IsOrigin = true,
                     Content = faker.Rant.Review(),
                     UserId = users[faker.Random.Int(0, users.Count - 1)].Id
                 };
-                var thread = new Thread
-                {
-                    ForumId = forums[faker.Random.Int(0, forums.Count - 1)].Id,
-                    Title =
-                        $"{faker.Music.Genre() + faker.Commerce.ProductMaterial() + faker.Name.JobTitle() + faker.Company.CompanyName()} {i}",
-                    Tags = tags.Skip(faker.Random.Int(3, 8)).ToList(),
-                    OriginPostId = originPost.Id
-                };
-                originPost.ThreadId = thread.Id;
                 context.Posts.Add(originPost);
                 context.Threads.Add(thread);
                 Console.WriteLine($"Thread: {thread.Title}");
