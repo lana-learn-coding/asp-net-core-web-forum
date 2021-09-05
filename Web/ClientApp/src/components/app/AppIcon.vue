56c3dc27a76e27341f863b46586bf73a44847877
toannd <sitdownrightnow.dev@gmail.com>
2021-09-05 21:04:33 +0700

    feat: update app icon

--- Web/ClientApp/src/components/app/AppIcon.vue
+++ Web/ClientApp/src/components/app/AppIcon.vue
@@ -1,5 +1,10 @@
 <template>
-  <span class="icon">Dr. Forum</span>
+  <v-hover #default="{ hover }">
+    <span :class="{icon: true, 'indigo--text': hover}" @click="$router.push({ name: 'Home' })">
+      <span class="mr-1">Dr. Forum</span>
+      <v-icon :color="hover ? 'indigo' : ''">masks</v-icon>
+    </span>
+  </v-hover>
 </template>
 
 <script>
@@ -8,12 +13,16 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .icon {
   font-size: 1.25rem;
   line-height: 1.5;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
+
+  &:hover {
+    cursor: pointer;
+  }
 }
 </style>
