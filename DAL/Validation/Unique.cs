afd6ec8e1d8643a22ab506879b24e1108b711218
toannd <sitdownrightnow.dev@gmail.com>
2021-09-06 04:41:43 +0700

    feat: add type param for unique validate

--- DAL/Validation/Unique.cs
+++ DAL/Validation/Unique.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Linq.Dynamic.Core;
 using DAL.Models;
 
@@ -6,16 +7,23 @@ namespace DAL.Validation
 {
     public class Unique : ValidationAttribute
     {
+        public Type Type { get; set; }
+
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var context = (ModelContext)validationContext.GetService(typeof(ModelContext));
             if (context == null) return ValidationResult.Success;
-            if (validationContext.ObjectInstance is not IIdentified identified) return ValidationResult.Success;
+            var type = Type ?? validationContext.ObjectType;
 
-            var type = validationContext.ObjectType;
-            var exist = context.Set(type)
-                .Any($"!Slug.Equals(@0) && {validationContext.MemberName}.Equals(@1)", identified.Slug, value);
-            return exist
+            if (validationContext.ObjectInstance is not IIdentified identified)
+            {
+                return context.Set(type).Any($"{validationContext.MemberName}.Equals(@0)", value)
+                    ? new ValidationResult($"The {validationContext.DisplayName} has already been taken")
+                    : ValidationResult.Success;
+            }
+
+            return context.Set(type).Any($"!Slug.Equals(@0) && {validationContext.MemberName}.Equals(@1)",
+                identified.Slug, value)
                 ? new ValidationResult($"The {validationContext.DisplayName} has already been taken")
                 : ValidationResult.Success;
         }
