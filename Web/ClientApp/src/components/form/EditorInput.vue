eb5d1b7de68746011c6bfe1526fe5d5169146103
toannd <sitdownrightnow.dev@gmail.com>
2021-09-08 21:09:37 +0700

    feat: use custom build ckeditor instead of prebuilt one
    1

--- Web/ClientApp/src/components/form/EditorInput.vue
+++ Web/ClientApp/src/components/form/EditorInput.vue
@@ -4,15 +4,39 @@
       <label
         class="v-label theme--light"
         style="left: 0; top: 0; position: absolute; font-size: 13px">{{ label }}</label>
-      <ckeditor :editor="editor" v-model="input"></ckeditor>
+      <ckeditor :editor="editor" :config="config" v-model="input"></ckeditor>
     </div>
   </v-input>
 </template>
 
 <script lang="ts">
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import Table from '@ckeditor/ckeditor5-table/src/table';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 import CKEditor from '@ckeditor/ckeditor5-vue2';
 import { computed, defineComponent } from '@vue/composition-api';
+import { useHttp } from '@/services/http';
 
 export default defineComponent({
   name: 'EditorInput',
@@ -27,13 +51,94 @@ export default defineComponent({
     ckeditor: CKEditor.component,
   },
 
-  setup(props, { emit }) {
+  data() {
     return {
       editor: ClassicEditor,
-      input: computed({
-        get: () => props.value,
-        set: (val) => emit('input', val),
-      }),
+    };
+  },
+
+  setup(props, { emit }) {
+    const input = computed({
+      get: () => props.value,
+      set: (val) => emit('input', val),
+    });
+
+    const http = useHttp();
+    const config = {
+      plugins: [
+        Essentials,
+        Autoformat,
+        Bold,
+        Italic,
+        BlockQuote,
+        Heading,
+        Image,
+        ImageCaption,
+        ImageStyle,
+        ImageToolbar,
+        ImageUpload,
+        Indent,
+        Link,
+        List,
+        MediaEmbed,
+        Paragraph,
+        PasteFromOffice,
+        Table,
+        TableToolbar,
+        TextTransformation,
+        SimpleUploadAdapter,
+        ImageResize,
+      ],
+      simpleUpload: {
+        uploadUrl: `${http.client.defaults.baseURL}/data/images`,
+        headers: {
+          Authorization: http.client.defaults.headers.Authorization ?? '',
+        },
+      },
+      toolbar: {
+        items: [
+          'heading',
+          '|',
+          'bold',
+          'italic',
+          'link',
+          'bulletedList',
+          'numberedList',
+          '|',
+          'outdent',
+          'indent',
+          '|',
+          'uploadImage',
+          'blockQuote',
+          'insertTable',
+          'mediaEmbed',
+          'undo',
+          'redo',
+        ],
+      },
+      image: {
+        toolbar: [
+          'imageStyle:inline',
+          'imageStyle:block',
+          'imageStyle:side',
+          '|',
+          'toggleImageCaption',
+          'imageTextAlternative',
+        ],
+      },
+      table: {
+        contentToolbar: [
+          'tableColumn',
+          'tableRow',
+          'mergeTableCells',
+        ],
+      },
+      language: 'en',
+    };
+
+    return {
+      input,
+      config,
     };
   },
 });
--- Web/ClientApp/vue.config.js
+++ Web/ClientApp/vue.config.js
@@ -1,7 +1,13 @@
-﻿module.exports = {
+﻿const path = require('path');
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
+const { styles } = require('@ckeditor/ckeditor5-dev-utils');
+
+module.exports = {
   transpileDependencies: [
     'vuetify',
+    /ckeditor5-[^/\\]+[/\\]src[/\\].+\.js$/,
   ],
+
   devServer: {
     proxy: {
       '^/api': {
@@ -9,5 +15,35 @@
         changeOrigin: true,
       },
     },
+  },
+
+  configureWebpack: {
+    plugins: [
+      new CKEditorWebpackPlugin({
+        language: 'en',
+        translationsOutputFile: /app/,
+      }),
+    ],
+  },
+
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+    svgRule.exclude.add(path.join(__dirname, 'node_modules', '@ckeditor'));
+    config.module
+      .rule('cke-svg')
+      .test(/ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/)
+      .use('raw-loader')
+      .loader('raw-loader');
+    config.module
+      .rule('cke-css')
+      .test(/ckeditor5-[^/\\]+[/\\].+\.css$/)
+      .use('postcss-loader')
+      .loader('postcss-loader')
+      .tap(() => styles.getPostCssConfig({
+        themeImporter: {
+          themePath: require.resolve('@ckeditor/ckeditor5-theme-lark'),
+        },
+        minify: true,
+      }));
   },
 };
