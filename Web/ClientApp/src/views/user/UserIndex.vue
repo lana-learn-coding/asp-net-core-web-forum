11e3b6470b603945f62d68a29cb0a04dae363132
toannd <sitdownrightnow.dev@gmail.com>
2021-09-09 17:56:20 +0700

    fix: fix speciality query

--- Web/ClientApp/src/views/user/UserIndex.vue
+++ Web/ClientApp/src/views/user/UserIndex.vue
@@ -132,7 +132,7 @@ export default defineComponent({
 
     const { query, data, meta } = useQuery<Dictionary>('users/infos')({
       search: '',
-      speciality: [],
+      speciality: '',
       city: '',
       country: '',
       position: '',
